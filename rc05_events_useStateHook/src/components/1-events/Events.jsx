e34483ef76a37c98292f898f58bb91687a4b4a7a
codesari <ahmtsr81@gmail.com>
2022-10-12 20:47:07 +0300

    react state

--- rc05_events_useStateHook/src/components/1-events/Events.jsx
+++ rc05_events_useStateHook/src/components/1-events/Events.jsx
@@ -8,6 +8,7 @@
 
 const Events = () => {
   // burasi JS ortami
+  let message = "EVENT BASED PROGRAM";
   // kisa ise inline uzun ise yukarida fonk tanimliyoruz (best practice)
   const handleClick = () => {
     alert("Button clicked.");
@@ -15,10 +16,17 @@ const Events = () => {
   const handleClear = (msg) => {
     alert(msg);
   };
+  const handleChange = (e) => {
+    console.log(e.target);
+    message = "REACT";
+    // react degiskenleri direk olarak DOM'a basmiyor.Durum Değişkneleri(State) ler burada devreye giriyor
+    console.log(message);
+  };
   return (
     // return ün ici JSX ortami
     <div>
       <div className="container text-center mt-4">
+        <h1>{message}</h1>
         {/* handle --> best practice */}
         {/* fonk. parametre almayacaksa bu sekilde kullanmak daha dogru. */}
         <button onClick={handleClick} className="btn btn-primary">
@@ -31,6 +39,11 @@ const Events = () => {
           className="btn btn-dark"
         >
           Clear
+        </button>
+        {/* burada handleChange'e parametre vermesek bile yukarida e'yi yakaladi */}
+        <button onClick={handleChange} className="btn btn-danger">
+          {/* <button onClick={(e) => handleChange(e)} className="btn btn-danger"> */}
+          Change
         </button>
       </div>
     </div>
