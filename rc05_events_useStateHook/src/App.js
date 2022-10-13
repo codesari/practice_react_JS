1af98b22380958cb712fe986f0933359c8589eac
codesari <ahmtsr81@gmail.com>
2022-10-13 15:49:29 +0300

    components exercises

--- rc05_events_useStateHook/src/App.js
+++ rc05_events_useStateHook/src/App.js
@@ -1,9 +1,12 @@
 import Events from "./components/1-events/Events";
+import Counter from "./components/2-classComponent/Counter";
 
 function App() {
   return (
     <div>
       <Events />
+      <Counter />
+      {/* <Counter count={5}/> */}
     </div>
   );
 }
---
+++ rc05_events_useStateHook/src/components/2-classComponent/Counter.jsx
@@ -0,0 +1,70 @@
+// import { Component } from "react";
+
+// class Counter extends Component {
+//   //rconst (snippet)
+//   constructor(props) {
+//     super(props);
+//     //count state'ine baslangic degeri atadik.
+//     this.state = {
+//       count: 0,
+//     };
+//     //? increment metodunun Counter class'ina baglanmasi (bind)
+//     this.increment = this.increment.bind(this);
+//   }
+//   // fonk. constructor'un disina yaziyoruz
+//   increment() {
+//     this.setState({
+//       count: this.state.count + 1,
+//     });
+//   }
+//   decrement = () => {
+//     //arrow fonk. ile kullandıgimizda binding yapmaya gerek kalmıyor
+//     this.setState({
+//       count: this.state.count - 1,
+//     });
+//   };
+
+//   render() {
+//     return (
+//       <div className="container text-center mt-4">
+//         <h1>CLASSFULL COMPONENTS</h1>
+//         <h2>COUNT :{this.state.count}</h2>
+//         <button onClick={this.increment} className="btn btn-success">
+//           INC
+//         </button>
+//         <button className="btn btn-danger">CLR</button>
+//         <button className="btn btn-primary">DEC</button>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Counter;
+
+import { Component } from "react";
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0,
+    };
+  }
+  render() {
+    return (
+      <div className="counter-div mt-4 text-center">
+        <h1>COUNTER COMPONENTS</h1>
+        {/* count degiskenin component icinde oldugu icin this ile erisiyoruz.ayrıca state'in icinde oldugu icin de this.state.count ile ulasabiliyoruz. */}
+        <h2 className="display-5">Count : {this.state.count}</h2>
+        <div className="button">
+          <button className="btn btn-success">Increment</button>
+          <button className="btn btn-danger">Decrement</button>
+          <button className="btn btn-warning">Reset</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Counter;
